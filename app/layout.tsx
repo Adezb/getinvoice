996df594248efe3baaba91cfa6ec442ef6626a32
Adezb <temitopeceo@gmail.com>
2025-05-10 11:56:01 +0100

    Fixed tailwindcss issues in package.json and postcss config file

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,3 +32,15 @@ export default function RootLayout({
     </html>
   );
 }
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
